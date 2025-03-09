746d3ed82e1dc71ac66bb03105daa37573b0c258
Mudit jain <muditj214@gmail.com>
2025-03-09 15:55:50 +0530

    Database Connected Successfully

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const dotenv = require('dotenv');
+const express = require('express');
+const mongoose = require('mongoose');
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+dotenv.config();
+const app = express();
+
+const port = process.env.PORT || 3000;
+const DB_URI = process.env.MONGO_URI;
+
+try {
+ mongoose.connect(DB_URI);
+ console.log('Connected to the database');
+}
+catch (error){
+    console.log(error);
+}
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Course selling app listening on port ${port}`)
 })
