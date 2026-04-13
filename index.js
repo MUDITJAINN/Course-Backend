4e817061b8d98d02161089f2522d6e3cd9f3b038
Mudit jain <muditj214@gmail.com>
2026-04-13 23:08:59 +0530

    Adding new proffesional Domain

--- index.js
+++ index.js
@@ -21,7 +21,11 @@ app.use(fileUpload({
 }));
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [
+      process.env.FRONTEND_URL,
+      process.env.FRONTEND_URL1, 
+      process.env.FRONTEND_URL2
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
